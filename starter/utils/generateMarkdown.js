7adcb2ba4f463209da28e861855530b1561796fb
Moses Opiah <moefolder@Mosess-MacBook-Pro.local>
2024-01-16 10:38:28 +0000

    generateMarkdown fuction tested

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,39 @@
-// function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
-`;
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+![License](https://img.shields.io/badge/License-${data.license}-blue.svg)
+
+This project is licensed under the ${data.license} License - see the [LICENSE](LICENSE) file for details.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For questions about the project, you can reach me at [GitHub: ${data.github}](https://github.com/${data.github}) or contact me at ${data.email}.
+  `;
 }
 
 module.exports = generateMarkdown;
